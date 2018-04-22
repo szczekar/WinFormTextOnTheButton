2fda5f23578b409f2e781c7cc119e03605dbfff3
Karol Szczepaniak <szczepaniakkarol@gmail.com>
2018-04-22 20:36:48 +0200

    Add files via upload

---
+++ WindowsFormsButtonTextbox/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsButtonTextbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputTextUser
+            // 
+            this.inputTextUser.Location = new System.Drawing.Point(267, 145);
+            this.inputTextUser.Name = "inputTextUser";
+            this.inputTextUser.Size = new System.Drawing.Size(249, 20);
+            this.inputTextUser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Put the text here";
+            // 
+            // button
+            // 
+            this.button.Location = new System.Drawing.Point(267, 207);
+            this.button.Name = "button";
+            this.button.Size = new System.Drawing.Size(249, 33);
+            this.button.TabIndex = 2;
+            this.button.Text = "Click and text from textBox here on the button";
+            this.button.UseVisualStyleBackColor = true;
+            this.button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputTextUser);
+            this.Name = "Form1";
+            this.Text = "ButtonTextBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button;
+    }
+}
+
